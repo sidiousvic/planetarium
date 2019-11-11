9dcdc8efdf1155eb25c514a16101768a87d3d2ca
sidiousvic <victor6rene@gmail.com>
2019-11-11 16:17:10 +0900

    Initial setup with React and Three.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Threehouse from './Threehouse';
+
+function App() {
+  return (
+    <div className="App">
+      <Threehouse />
+    </div>
+  );
+}
+
+export default App;
