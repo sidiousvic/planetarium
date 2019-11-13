a4bac174090bbea3faf03260c077edc68ddcc362
sidiousvic <victor6rene@gmail.com>
2019-11-13 12:15:54 +0900

    added more textures

--- src/assets/3D/textures/planetTextures.js
+++ src/assets/3D/textures/planetTextures.js
@@ -2,12 +2,13 @@ const mercuryTexture = require('./mercuryTexture.jpg');
 // const mercuryBumpMap = require('./mercuryBumpMap.jpg');
 const venusTexture = require('./venusTexture.jpg');
 const earthTexture = require('./earthTexture.jpg');
-// const earthNightTexture = require('./earthNightTexture.jpg');
-// const earthBumpMap = require('./earthBumpMap.jpg');
+const earthCloudTexture = require('./earthCloudTexture.jpg');
+const earthSpecularMap = require('./earthSpecularMap.tif');
 const marsTexture = require('./marsTexture.jpg');
 const marsBumpMap = require('./marsBumpMap.png');
 const jupiterTexture = require('./jupiterTexture.jpg');
 const saturnTexture = require('./saturnTexture.jpg');
+const saturnRingTexture = require('./saturnRingTexture.png');
 const uranusTexture = require('./uranusTexture.jpg');
 const neptuneTexture = require('./neptuneTexture.jpg');
 // const plutoTexture = require('./plutoTexture.jpg');
@@ -23,7 +24,9 @@ const textures = {
   },
   earth: {
     texture: earthTexture,
-    bumpMap: earthTexture
+    bumpMap: earthTexture,
+    cloudTexture: earthCloudTexture,
+    specularMap: earthSpecularMap
   },
   mars: {
     texture: marsTexture,
@@ -35,7 +38,7 @@ const textures = {
   },
   saturn: {
     texture: saturnTexture,
-    bumpMap: saturnTexture
+    ringTexture: saturnRingTexture
   },
   uranus: {
     texture: uranusTexture,
